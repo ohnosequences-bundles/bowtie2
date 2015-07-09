4d1cae195eca6166839df2e9c85e8302afbd3bd5
Marina Manrique <mmanrique@era7.com>
2015-07-09 12:59:59 +0200

    update build.sbt with dependencies and bowtie2.scala with the install method

--- src/main/scala/bowtie2.scala
+++ src/main/scala/bowtie2.scala
@@ -3,10 +3,17 @@ package ohnosequencesBundles.statika
 import ohnosequences.statika._, bundles._, instructions._
 
 
-case object bowtie2 extends Bundle() {
+abstract class bowtie2(val version: String) extends Bundle() {
+
+	import ammonite.ops._
+
+	val usrbin = root/"usr"/"bin"
 
   def install: Results = {
-    // do someting here
+    Seq("aws", "s3", "cp", s"s3://resources.ohnosequences.com/bowtie2/${version}/bowtie2-${version}-linux-x86_64.zip", "-O", "bowtie2-${version}-linux-x86_64.zip") ->-
+    Seq("unzip", s"bowtie2-${version}-linux-x86_64.zip") ->-
+    Seq("cd", s"bowtie2-${version}") ->-
+    Seq("cp", "bowtie2", "bowtie2-*", "/usr/bin/") ->-
     success(s"${bundleName} is installed")
   }
 
