bd99cde7e476cceb763e1a78b9086eeff379910e
Marina Manrique <mmanrique@era7.com>
2015-07-29 11:03:11 +0200

    minor fix

--- src/main/scala/bowtie2.scala
+++ src/main/scala/bowtie2.scala
@@ -10,7 +10,7 @@ abstract class bowtie2(val version: String) extends Bundle() {
 	val usrbin = root/"usr"/"bin"
 
   def install: Results = {
-    Seq("aws", "s3", "cp", s"s3://resources.ohnosequences.com/bowtie2/${version}/bowtie2-${version}-linux-x86_64.zip", "-O", "bowtie2-${version}-linux-x86_64.zip") ->-
+    Seq("aws", "s3", "cp", s"s3://resources.ohnosequences.com/bowtie2/${version}/bowtie2-${version}-linux-x86_64.zip", "-O", s"bowtie2-${version}-linux-x86_64.zip") ->-
     Seq("unzip", s"bowtie2-${version}-linux-x86_64.zip") ->-
     Seq("cd", s"bowtie2-${version}") ->-
     Seq("cp", "bowtie2", "bowtie2-*", "/usr/bin/") ->-
